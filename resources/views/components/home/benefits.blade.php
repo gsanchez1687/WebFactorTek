f32a1c2c660991d3ea864e593869d27bb2f4c00a
Guillermo Enrique <gsanchez1687@gmail.com>
2023-04-23 17:30:37 -0400

    Update scss style characteristics and benefits

--- resources/views/components/home/benefits.blade.php
+++ resources/views/components/home/benefits.blade.php
@@ -6,7 +6,7 @@
             <div class="row mt-4">
                 @for ($i = 1; $i < count($data['data']); $i++)
                 <div class="col-md-4">
-                    <div class="card" style="width: 22rem;height:26rem">
+                    <div class="card">
                         <img src="{{ $data['data'][$i]['imageheader'] }}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <img width="100px" src="{{ $data['data'][$i]['imagemini'] }}" class="imagemini" alt="...">
--- resources/views/components/home/characteristics.blade.php
+++ resources/views/components/home/characteristics.blade.php
@@ -6,17 +6,17 @@
             <h2 class="description-style">{{ $data['data'][0]['description'] }}</h2>
          @endif
          <div class="row mt-4">
-            <div class="col-md-6">
+            <div class="col-md-6 col-sm-12">
                 <div class="card">
-                    <div class="card-body">
+                    <div style="height: 10%;" class="card-body">
                         @if ($data['data'][1]['category'] == 2)
                             <div class="row">
-                                <div class="col-md-6 col-sm-6">
+                                <div class="col-md-6 col-sm-12 col-xs-12">
                                    <img class="img-fluid" alt="funding" src="{{ $data['data'][1]['image'] }}">
                                 </div>
                                 <div class="col-md-6 col-sm-6 text-funding">
                                     <h3>{{ $data['data'][1]['title'] }}</h3>
-                                    <h4> {{ ($data['data'][1]['description'])  }} </h4>
+                                    <h4>{{ ($data['data'][1]['description']) }}</h4>
                                     
                                 </div>
                             </div>
@@ -26,15 +26,15 @@
             </div>
             <div class="col-md-6">
                 <div class="card">
-                    <div class="card-body">
+                    <div style="height: 10%;" class="card-body">
                       @if ($data['data'][2]['category'] == 2)
                         <div class="row">
-                            <div class="col-md-6 col-sm-6">
+                            <div class="col-md-6 col-sm-12 col-xs-12">
                             <img class="img-fluid" alt="funding" src="{{ $data['data'][2]['image'] }}">
                             </div>
                             <div class="col-md-6 col-sm-6 text-funding">
                                 <h3>{{ $data['data'][2]['title'] }}</h3>
-                                <h4> {{ ($data['data'][2]['description'])  }} </h4>
+                                <h4>{{ ($data['data'][2]['description']) }}</h4>
                             </div>
                         </div>
                     @endif
