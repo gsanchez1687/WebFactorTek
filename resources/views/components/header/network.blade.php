90623b3cd31afc024245a47278f946e4172d18da
Guillermo Enrique <gsanchez1687@gmail.com>
2023-04-24 16:21:40 -0400

    add lang, fix error with menu z-index

--- resources/views/components/header/network.blade.php
+++ resources/views/components/header/network.blade.php
@@ -2,6 +2,15 @@
     <div class="row">
         <div class="col-md-12 animate__animated animate__fadeIn">
             <div class="network-icon text-white d-flex justify-content-end mx-4">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Lang
+                      </a>
+                      <ul class="dropdown-menu bg-dark bg-gradient">
+                        <li><a class="dropdown-item" href="#">ES</a></li>
+                        <li><a class="dropdown-item" href="#">EN</a></li>
+                      </ul>
+                </li>
                 @for ($i = 0; $i < count($data['data']); $i++)
                     <span class="icons-style"><a href="{{ $data['data'][$i]['link'] }}"><i class="{{ $data['data'][$i]['icon'] }}"></i></a></span>    
                 @endfor
