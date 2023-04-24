32b05339c7bbcaf048c94a832648b8ce0ec824e6
Guillermo Enrique <gsanchez1687@gmail.com>
2023-04-24 15:42:48 -0400

    update menu to menusidebar

--- resources/views/components/header/menu.blade.php
+++ resources/views/components/header/menu.blade.php
@@ -1,14 +1,23 @@
-<div class="menu animate__animated animate__fadeIn">
+<div id="menu" class="animate__animated animate__fadeIn">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="col-lg-4">
             <a class="navbar-brand" href="#"> <span class="factor-style">factor</span><span class="tek-style">tek</span> </a>
         </div>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-                <div style="padding-right:9%;" class="col-lg-12">
+        
+        
+        <div class="offcanvas offcanvas-start" id="navbarSupportedContent">
+            <div class="offcanvas-header">
+                <div class="col-lg-4">
+                    <a class="navbar-brand" href="#"> <span class="factor-style">factor</span><span class="tek-style">tek</span> </a>
+                </div>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+              </div>
+            <div style="padding-right:10%" class="col-lg-12">
+                <div class="offcanvas-body">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-end">
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
@@ -33,6 +42,8 @@
                         </li>
                     </ul>
                 </div>
+                
+            </div>
         </div>
     </nav>
 </div>
